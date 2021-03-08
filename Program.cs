df5875939e28e6f7b57302cba706e47b81304790
wlgnsdl815 <80227700+wlgnsdl815@users.noreply.github.com>
2021-03-08 10:00:37 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CSharp world!!!");
+            //Console.ReadLine();
+            Console.WriteLine("학번 이름");
+        }
+    }
+}
