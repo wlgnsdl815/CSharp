01d031e7d684ce120f333651803fb39e7272f43d
wlgnsdl815 <80227700+wlgnsdl815@users.noreply.github.com>
2021-03-15 10:49:06 +0900

    Add files via upload

---
+++ ArrayOfArrayApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArrayOfArrayApp
+{
+    class ArrayOfArrayApp
+    {
+        static void Main(string[] args)
+        {
+            int[][] arrayOfArray = new int[3][];
+            int i, j;
+            for (i = 0; i < arrayOfArray.Length; i++)
+                arrayOfArray[i] = new int[i + 3];
+            for (i = 0; i < arrayOfArray.Length; i++)
+                for(j=0; j< arrayOfArray[i].Length; j++)
+                arrayOfArray[i][j] = i * arrayOfArray[i].Length + j;
+            for ( i = 0; i < arrayOfArray.Length; i++)
+            {
+                for(j = 0; j < arrayOfArray[i].Length; j++)
+                    Console.WriteLine(" " + arrayOfArray[i][j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
